ad493410994387fd180f8b7163704ae7c7c04f1c
georgefox831 <honest.dev0104@gmail.com>
2022-01-11 16:57:28 +0530

    Sync-in changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    
+    flash[:notice] = "Logged in Successfully"
+    flash[:alert] = "Invalid email or Password"
+
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 end
