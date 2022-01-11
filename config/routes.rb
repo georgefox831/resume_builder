afedd3aaa8850fefa962679c8c0e1f27ab7a83f5
georgefox831 <honest.dev0104@gmail.com>
2022-01-11 22:39:13 +0530

    error fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  # root 'home#index'
+  # get 'home/index'
+  root 'home#index'
   get 'home/about'
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
