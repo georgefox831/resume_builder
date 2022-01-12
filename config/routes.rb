def70fb0c4c40d61169ff376d1eb4dcba7d37f46
georgefox831 <honest.dev0104@gmail.com>
2022-01-12 09:02:07 +0530

    Routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  # root 'home#index'
   get 'home/about'
+  root "registrations#new"
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
   delete "logout" , to: "sessions#destroy"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
